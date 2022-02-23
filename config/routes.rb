fbf6e5ee58d0883149dfd923aa8ddda6cbbd84fd
Jeremiah <jeremiahmarcos27@outlook.com>
2022-02-23 16:34:15 -0600

    seeded db

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api do
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   get '/hello', to: 'application#hello_world'
+  end
 end
