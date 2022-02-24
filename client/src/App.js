ad82f313f9f5607b5d517af403a1c589cacfe18c
Jeremiah <jeremiahmarcos27@outlook.com>
2022-02-24 17:26:26 -0600

    added the components and words are showing

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,33 @@
 import { useState, useEffect } from 'react'
 import './App.css';
+import Game from './components/Game'
+import Result from './components/Result'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [words, setWords] = useState([])
+  const [start, setStart] = useState(false)
 
   useEffect(() => {
-    fetch("/hello")
+    fetch("https://random-word-api.herokuapp.com/word?number=10")
       .then((r) => r.json())
-      .then((data) => setCount(data.count));
-  }, []);
+      .then((word) => setWords(word));
+  }, [start]);
 
 
+  const start_the_game = () => {
+    setStart(prev => !prev)
+  }
+  const typeRacer = words.map((elem) => {
+    return (<Game elem={elem} />)
+  })
   return (
     <div className="App">
-      <h1>Page Count: {count}</h1>
+      <h1>Type Racing Game</h1>
+      <h2>Jeremiah Marcos</h2>
+      <Result />
+      {start ? typeRacer : ""}
+
+      <button onClick={start_the_game}>{start ? "STOP" : "START"}</button>
     </div>
   );
 }
---
+++ client/src/components/Game.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Game({ elem }) {
+    console.log(elem)
+
+    return (
+        <h3>{elem}</h3>
+        // <h1>{elem}</h1>
+    )
+}
+
+export default Game
