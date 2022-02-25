5d70cec4808f3dd86506d5c8ad3a86d86768f3d1
jmarcos00312 <jeremiahmarcos27@outlook.com>
2022-02-25 15:08:13 -0600

    input disability working, onKeyPress working, but not perfectly

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './game.css'
 
 
@@ -9,13 +9,19 @@ function Game({ words, start, setStart }) {
     const [wrongResults, setWrongResults] = useState([])
     const [time, setTime] = useState(30)
     // let single_word = word
-
-
     let random_index = Math.floor(Math.random() * words.length)
+
+    const newWord = () => {
+        return start && setCurrentWord[random_index]
+    }
+
+    start && newWord()
+
 
     const check_input_if_match = () => {
         if (inputValue.trim() === currentWord) {
             setCorrectResults([...correctResults, inputValue])
+            return
         }
         else {
             setWrongResults([...wrongResults, inputValue])
@@ -30,18 +36,19 @@ function Game({ words, start, setStart }) {
             check_input_if_match()
             setCurrentWord(words[random_index])
             setInputValue("")
+            console.log('clicked enter')
         }
 
     }
 
-    const handleStart = () => {
-        setCurrentWord(words[random_index])
-        setStart(prev => !prev)
+    const handleChange = (e) => {
+        setInputValue(e.target.value)
+        console.log('====================================');
+        console.log(inputValue);
+        console.log('====================================');
     }
 
-
-    //TODO: add a for loop that loops thru the array of words to show 1 at a time
-    //TODO: add input field to type each words
+//
     //TODO: add the word to either correct or wrong array to show the result
     //need to add a for loop
     const text_field = words.map((word) => {
@@ -53,16 +60,17 @@ function Game({ words, start, setStart }) {
         <div className="game-container">
 
             <div className="input-word-display">
-                <h3 id="word-display">{words[random_index]}</h3>
+
+                <h3 id="word-display">{currentWord}</h3>
             </div>
 
             <input
                 type="text"
-                disabled={!start && !start}
+                disabled={!start}
                 onKeyPress={e => handleInputValue(e)}
                 value={inputValue}
-            // onChange={(e) => setInputValue(e.target.value)}
-            // placeholder={disabled ? "" : "Start Typing..."}
+                onChange={(e) => handleChange(e)}
+                placeholder={start ? "" : "Start Typing..."}
             />
 
         </div>
