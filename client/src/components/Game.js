d75a881c0cbb72d84f3f2e184c63e53d523b0422
jmarcos00312 <jeremiahmarcos27@outlook.com>
2022-02-25 14:44:02 -0600

    added input value, handle start, event listener and check if input mwatch

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -13,12 +13,32 @@ function Game({ words, start, setStart }) {
 
     let random_index = Math.floor(Math.random() * words.length)
 
-    console.log(random_index)
-
-
-    const check_input = () => {
+    const check_input_if_match = () => {
+        if (inputValue.trim() === currentWord) {
+            setCorrectResults([...correctResults, inputValue])
+        }
+        else {
+            setWrongResults([...wrongResults, inputValue])
+        }
 
     }
+    // console.log(random_index)
+
+
+    const handleInputValue = (e) => {
+        if (e.charCode === 13 && inputValue.trim() !== '') {
+            check_input_if_match()
+            setCurrentWord(words[random_index])
+            setInputValue("")
+        }
+
+    }
+
+    const handleStart = () => {
+        setCurrentWord(words[random_index])
+        setStart(prev => !prev)
+    }
+
 
     //TODO: add a for loop that loops thru the array of words to show 1 at a time
     //TODO: add input field to type each words
@@ -38,9 +58,9 @@ function Game({ words, start, setStart }) {
 
             <input
                 type="text"
-            disabled={!start && !start}
-            // onKeyPress={e => handleInput(e)}
-            // value={inputValue}
+                disabled={!start && !start}
+                onKeyPress={e => handleInputValue(e)}
+                value={inputValue}
             // onChange={(e) => setInputValue(e.target.value)}
             // placeholder={disabled ? "" : "Start Typing..."}
             />
