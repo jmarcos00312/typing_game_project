903c0d9dc75ba7b582dffabab822c224bd34d7fc
jmarcos00312 <jeremiahmarcos27@outlook.com>
2022-02-27 09:12:32 -0600

    started to fix the timer

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -9,6 +9,7 @@ function Game({ start, setStart }) {
     const [correctResults, setCorrectResults] = useState([])
     const [wrongResults, setWrongResults] = useState([])
     const [time, setTime] = useState(30)
+    const [isTimeRunning, setIsTimeRunning] = useState(false)
     // let single_word = word
 
     let random_index = Math.floor(Math.random() * words.length)
@@ -17,12 +18,18 @@ function Game({ start, setStart }) {
         fetch("https://random-word-api.herokuapp.com/word?number=100")
             .then((r) => r.json())
             .then((word) => {
+
                 setWords(word)
                 setCurrentWord(words[random_index])
+                setIsTimeRunning(prev => !prev)
             });
     }, [start]);
 
-
+    useEffect(() => {
+        if (start && time !== 0) {
+            setTimeout(() => setTime(prevTime => prevTime - 1), 1000)
+        }
+    }, [])
     // console.log(generateWord)
     const check_input_if_match = () => {
         if (inputValue.trim() === currentWord) {
@@ -52,18 +59,12 @@ function Game({ start, setStart }) {
     const displayWrong = wrongResults.map(item => {
         return (<li>{item}</li>)
     })
-    const runTime = () => {
-        start && setTime(prev => prev--)
-        console.log(time)
-    }
+    // runTime()
     // start && setTime(prev => prev--)
     //TODO: display words on start
     //TODO: add time feature
     //TODO: calculate scores
     //TODO: add the word to either correct or wrong array to show the result
-    //need to add a for loop
-
-    // console.log(text_field)
     return (
         <div className="game-container">
 
