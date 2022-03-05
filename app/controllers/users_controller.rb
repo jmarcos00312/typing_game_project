fb5d9a13b8fd133c795d60069052694e74a475dd
jmarcos00312 <jeremiahmarcos27@outlook.com>
2022-03-04 22:11:18 -0600

    need to fix leaderboards from highest to lowest

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   # GET /users
   def index
-    render json: User.order('score ASC').limit(10)
+    render json: User.order(score: :asc).limit(10)
   end
 
   # GET /users/1
